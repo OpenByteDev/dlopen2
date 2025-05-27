570b4f8eb1a863f7591187778a8f76e85cc4aadc
OpenByte <development.openbyte@gmail.com>
2025-05-27 22:41:31 +0200

    Fix #17

--- dlopen2/src/raw/tests.rs
+++ dlopen2/src/raw/tests.rs
@@ -1,10 +1,9 @@
 use super::super::err::Error;
+use std::ffi::CStr;
 #[cfg(unix)]
 use super::unix::{close_lib, get_sym, open_lib};
 #[cfg(windows)]
 use super::windows::{close_lib, get_sym, open_lib};
-
-use const_cstr::const_cstr;
 
 #[cfg(windows)]
 const EXISTING_LIB: &str = "kernel32.dll";
@@ -14,10 +13,10 @@ const EXISTING_LIB: &str = "libm.so.6";
 const EXISTING_LIB: &str = "libm.dylib";
 const NOT_EXISTING_LIB: &str = "notexisting.ext";
 #[cfg(windows)]
-const_cstr! {EXISTING_SYM = "GetLastError";}
+const EXISTING_SYM: &CStr = c"GetLastError";
 #[cfg(unix)]
-const_cstr! {EXISTING_SYM = "cos";}
-const_cstr! {NOT_EXISTING_SYM = "notexisting";}
+const EXISTING_SYM: &CStr = c"cos";
+const NOT_EXISTING_SYM : &CStr= c"notexisting";
 
 //This is an example of opening and closing a library
 //It's going to work only on Windows but this is what it is supposed to do
@@ -25,7 +24,7 @@ const_cstr! {NOT_EXISTING_SYM = "notexisting";}
 fn load_get_close() {
     unsafe {
         let handle = open_lib(EXISTING_LIB.as_ref(), None).expect("Could not open library");
-        let sym = get_sym(handle, EXISTING_SYM.as_cstr()).expect("Could not get symbol");
+        let sym = get_sym(handle, EXISTING_SYM).expect("Could not get symbol");
         assert!(!sym.is_null());
         assert!(close_lib(handle).is_null());
     }
@@ -48,7 +47,7 @@ fn open_err() {
 fn get_err() {
     unsafe {
         let handle = open_lib(EXISTING_LIB.as_ref(), None).expect("Could not open library");
-        match get_sym(handle, NOT_EXISTING_SYM.as_cstr()) {
+        match get_sym(handle, NOT_EXISTING_SYM) {
             Ok(_) => panic!("Should not get the symbol"),
             Err(err) => match err {
                 Error::SymbolGettingError(_) => (),
