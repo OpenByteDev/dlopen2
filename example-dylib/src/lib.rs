db4a7c6b0a08804ddb793be335011d55c04d7e05
OpenByte <development.openbyte@gmail.com>
2025-12-06 19:41:35 +0100

    Fix build of example_dylib

--- example-dylib/src/lib.rs
+++ example-dylib/src/lib.rs
@@ -7,41 +7,41 @@
 use std::os::raw::{c_char, c_int};
 
 //FUNCTIONS
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn rust_fun_print_something() {
     println!("something");
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn rust_fun_add_one(arg: i32) -> i32 {
     arg + 1
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn c_fun_print_something_else() {
     println!("something else");
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn c_fun_add_two(arg: c_int) -> c_int {
     arg + 2
 }
 
 #[allow(unused_variables)]
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn c_fun_variadic(txt: *const c_char) {
     //pretend to be variadic - impossible to do in Rust code
 }
 
 //STATIC DATA
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub static mut rust_i32_mut: i32 = 42;
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub static rust_i32: i32 = 43;
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub static mut c_int_mut: c_int = 44;
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub static c_int: c_int = 45;
 
 #[repr(C)]
@@ -50,7 +50,7 @@ pub struct SomeData {
     second: c_int,
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub static c_struct: SomeData = SomeData {
     first: 1,
     second: 2,
@@ -60,8 +60,8 @@ pub static c_struct: SomeData = SomeData {
 
 //exporting str directly is not so easy - it is not Sized!
 //you can only export a reference to str and this requires double dereference
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub static rust_str: &str = "Hello!";
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub static c_const_char_ptr: [u8; 4] = [b'H', b'i', b'!', 0];
